e4f3519d6a7f891e6909618fd364b829b2fd4c2e
Petrarkius <greeinq@gmail.com>
2023-08-25 16:50:53 +0300

    Added get_spectators and get_artists

--- classes/NeuroEventBot.py
+++ classes/NeuroEventBot.py
@@ -1,7 +1,10 @@
+from typing import Union
 from discord.ext import commands
 from classes.Texts import Texts
 from classes.EventStage import EventStage
 from classes.VoteList import ListCategory
+from discord.ext.commands import Context
+from discord import User, Member
 
 class NeuroEventBot(commands.Bot):
     current_stage: EventStage
@@ -10,7 +13,7 @@ class NeuroEventBot(commands.Bot):
     
     organizers: list[int]
     art_dict: dict[str, str]
-    art_top: dict[str, dict[str, int]]
+    # art_top: dict[str, dict[str, int]]
     top_lists: dict[int, dict[ListCategory, dict[int, str]]]
     
     
@@ -26,8 +29,28 @@ class NeuroEventBot(commands.Bot):
         self.art_dict = {}
         self.spectators = []
         self.spectators_msg_id = None
-        self.art_top = {}
+        # self.art_top = {}
         self.top_lists = {}
         
     async def setup_hook(self):
         await self.tree.sync()
+    
+    async def get_spectators(self, ctx: Context) -> list[Union[User, Member]]:
+        sm_id = self.spectators_msg_id
+        if sm_id == None:
+            raise ValueError('There is no msg id, where reactions can be obtained!')
+        
+        spectators_msg = await ctx.channel.fetch_message(sm_id)
+        SPECTATOR_EMOJI_IND = 0
+        spectators_async_iter = spectators_msg.reactions[SPECTATOR_EMOJI_IND].users()
+        spectators = [
+            spectator 
+            async for spectator in spectators_async_iter 
+            if spectator.id not in self.art_dict
+        ]
+        
+        return spectators
+    
+    def get_artists(self):
+        return list(self.art_dict.keys())
+        
