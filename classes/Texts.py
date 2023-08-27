e275b09e706a2611d51ea8e8a8189453b225d432
tovsmax <tovsmax@gmail.com>
2023-08-27 20:06:40 +0300

    Fixed unreadable check marks

--- classes/Texts.py
+++ classes/Texts.py
@@ -4,7 +4,7 @@ class Texts:
     
     GATHERING_ART_IS_NOT_ATTACHED = '❌ Арт не прикреплён в сообщении!'
     GATHERING_ART_TITLE_IS_NOT_PROVIDED = '❌ Название арта не указано в сообщении!'
-    GATHERING_ART_IS_ACCEPTED = '✔️ Арт был внесён в нейроивент. Спасибо за участие ^^'
+    GATHERING_ART_IS_ACCEPTED = '✅ Арт был внесён в нейроивент. Спасибо за участие ^^'
 
     VOTING_GATHERING_NOT_STARTED = '❌ Участники ивента пока не начали скидывать свои работы!'
     VOTING_STARTED = 'Бланки списков разосланы.'
@@ -17,6 +17,6 @@ class Texts:
     VOTING_BUTTON_LABEL = 'Отправить рейтинг'
     VOTING_MULTIPLE_RANK = '❌ Один и тот же арт не должен занимать разные места!'
     VOTING_EMPTY_RANK = '❌ Не все арты были оценены!'
-    VOTING_REPLY = '✔️ Ваш список отправлен!'
+    VOTING_REPLY = '✅ Ваш список отправлен!'
     
     FINISH_NOT_ALL_VOTED = '❌ Проголосовало только {0} из {1}! Если не хотите ждать всех, то передай в параметр `force_finish` значение "True".'
